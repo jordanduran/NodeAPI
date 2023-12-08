47a25386d413f4bd5bd8214f777603918a7d5c80
Jordan Duran <jordanduranm@gmail.com>
2023-12-08 13:24:36 -0500

    Updating a product by id

--- server.js
+++ server.js
@@ -4,15 +4,12 @@ const Product = require('./models/productModel');
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // routes
 
 app.get('/', (req, res) => {
   res.send('Hello Node API');
-});
-
-app.get('/blog', (req, res) => {
-  res.send('Hello Blog my name is Jordan');
 });
 
 app.get('/products', async (req, res) => {
@@ -38,6 +35,25 @@ app.post('/products', async (req, res) => {
   try {
     const product = await Product.create(req.body);
     res.status(200).json(product);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Updating a product
+
+app.put('/products/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
+    if (!product) {
+      return res
+        .status(404)
+        .json({ message: `Product with ID: ${id} does not exist` });
+    }
+    const updatedProduct = await Product.findById(id);
+    res.status(200).json(updatedProduct);
   } catch (error) {
     console.log(error.message);
     res.status(500).json({ message: error.message });
