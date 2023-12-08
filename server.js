f19b954a6c0ade9421b60aea51a99aa04b3dace4
Jordan Duran <jordanduranm@gmail.com>
2023-12-08 12:56:06 -0500

    Connecting to mongoDB and creating productSchema

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
 // routes
@@ -7,6 +8,21 @@ app.get('/', (req, res) => {
   res.send('Hello Node API');
 });
 
-app.listen(3000, () => {
-  console.log('Node API app is running on PORT 3000');
+app.get('/blog', (req, res) => {
+  res.send('Hello Blog my name is Jordan');
 });
+
+mongoose.set('strictQuery', false);
+mongoose
+  .connect(
+    'mongodb+srv://admin:admin821@nodeapi.5xj24uq.mongodb.net/node-api?retryWrites=true&w=majority'
+  )
+  .then(() => {
+    console.log('Successfully connected to MongoDB');
+    app.listen(3000, () => {
+      console.log('Node API app is running on PORT 3000');
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
